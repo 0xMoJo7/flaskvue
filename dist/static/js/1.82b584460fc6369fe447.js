981c49ec8d355223145ef50baca664295b6bd564
emdavis7 <ericd@simpli.fi>
2017-11-14 16:19:23 -0600

    add frontend

---
+++ dist/static/js/1.82b584460fc6369fe447.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{c27y:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},l=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("p",[e._v("About")])])}],u={render:r,staticRenderFns:l},c=u,s=n("VU/8"),a=s(null,c,!1,null,null,null);t.default=a.exports}});
+//# sourceMappingURL=1.82b584460fc6369fe447.js.map
